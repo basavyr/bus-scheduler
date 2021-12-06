d0089467e635adab6aa53b7aa31878b8d98bad88
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-12-06 20:22:29 +0200

    Update times.py

--- src/times.py
+++ src/times.py
@@ -5,13 +5,51 @@ from datetime import datetime
 import time
 
 
-def Execute_Main_Procedure():
+def CreateDateTimeFromInput(hour, minutes):
+    try:
+        # create a datetime object from the input hour and minutes
+        now = datetime.now()
+        dateTime = datetime(now.year, now.month, now.day, hour, minutes)
+        return dateTime
+    except Exception as e:
+        print(f'{e} ❌')
 
+
+def ReadDateTimeObjects(file):
+    # read the hour and minutes from the file
+    try:
+        # read the file and create a list of datetime objects
+        with open(file, 'r+') as f:
+            lines = f.readlines()
+            dateTimeList = []
+            for line in lines:
+                hour, minutes = line.split(':')
+                dateTime = CreateDateTimeFromInput(int(hour), int(minutes))
+                dateTimeList.append(dateTime)
+            return dateTimeList
+    except Exception as e:
+        print(f'{e} ❌')
+
+
+def GetUNIXTimestamp(timestamp_obj):
+    # get UNIX timestamp from a datetime object
+    try:
+        timestamp = time.mktime(timestamp_obj.timetuple())
+        return timestamp
+    except Exception as e:
+        print(f'{e} ❌')
+
+
+def Execute_Main_Procedure():
+    # execute the main procedure
     time_list = ReadDateTimeObjects('hours.txt')
-    print(time_list)
-    # print('evaluating time elements')
-    # for time in time_list:
-    #     print(CreateDateTimeFromInput(hour, minutes))
+    print('evaluating time elements')
+    for time in time_list:
+        hour = now.hour
+        minutes = now.minute
+        print(GetUNIXTimestamp(time))
+    #get current tiem in epoch
+    print(GetUNIXTimestamp(now))
 
 
 if __name__ == '__main__':
@@ -23,32 +61,5 @@ if __name__ == '__main__':
         # execute the main procedure of the codebase
         Execute_Main_Procedure()
 
-    except Exception as e:
-        print(f'{e} ❌')
-
-
-def CreateDateTimeFromInput(hour, minutes):
-    try:
-        # create a datetime object from the input hour and minutes
-        now=datetime.now()
-        dateTime = datetime(now.year, now.month, now.day, hour, minutes)
-        return dateTime
-    except Exception as e:
-        print(f'{e} ❌')
-
-# read the hour and minutes from the file
-
-
-def ReadDateTimeObjects(file):
-    try:
-        # read the file and create a list of datetime objects
-        with open(file, 'r+') as f:
-            lines = f.readlines()
-            dateTimeList = []
-            for line in lines:
-                hour, minutes = line.split(':')
-                dateTime = CreateDateTimeFromInput(int(hour), int(minutes))
-                dateTimeList.append(dateTime)
-            return dateTimeList
     except Exception as e:
         print(f'{e} ❌')
